f123fa01dd48352f8441f7e4563efad33fcca4cb
Casey Capetola <caseycapetola@gmail.com>
2023-09-07 11:18:30 -0400

    Added probability calculation

--- src/IsingModelVisualizer.java
+++ src/IsingModelVisualizer.java
@@ -4,16 +4,21 @@ import java.util.Random;
 
 public class IsingModelVisualizer extends JFrame {
     private static final long serialVersionUID = 1L;
-	private static final int GRID_SIZE = 150; // Change this to your desired grid size
-    private static final double BLUE_THRESHOLD = 0.5; // Adjust this threshold as needed
+	private static final int GRID_SIZE = 300;
+    private static final double BLUE_THRESHOLD = 0.5;
+    private static final int BETA = 10;
+    private static final int POS = 1;
+    private static final int NEG = -1;
 
     private JPanel[][] gridPanels;
+    private int[][] grid;
     private Random random;
     private int xcoord, ycoord;
 
     public IsingModelVisualizer() {
         random = new Random();
         gridPanels = new JPanel[GRID_SIZE][GRID_SIZE];
+        grid = new int[GRID_SIZE][GRID_SIZE];
         initializeGUI();
         startPixelUpdate();
     }
@@ -23,14 +28,31 @@ public class IsingModelVisualizer extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(GRID_SIZE, GRID_SIZE));
 
+//        for (int i = 0; i < GRID_SIZE; i++) {
+//            for (int j = 0; j < GRID_SIZE; j++) {
+//                gridPanels[i][j] = new JPanel();
+//                if(j<75) {
+//                	gridPanels[i][j].setBackground(Color.BLUE); // Initial color is blue
+//                	grid[i][j] = NEG;
+//            	}
+//                else {
+//                	gridPanels[i][j].setBackground(Color.RED); // Initial color is red
+//                	grid[i][j] = POS;
+//                }
+//                add(gridPanels[i][j]);
+//            }
+//        }
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
                 gridPanels[i][j] = new JPanel();
-                if(j<75) {
+                gridPanels[i][j].setPreferredSize(new Dimension(2,2));
+                if(j%2==0 || i%2==0) {
                 	gridPanels[i][j].setBackground(Color.BLUE); // Initial color is blue
+                	grid[i][j] = NEG;
             	}
                 else {
                 	gridPanels[i][j].setBackground(Color.RED); // Initial color is red
+                	grid[i][j] = POS;
                 }
                 add(gridPanels[i][j]);
             }
@@ -42,7 +64,7 @@ public class IsingModelVisualizer extends JFrame {
     }
 
     private void startPixelUpdate() {
-        Timer timer = new Timer(1000, e -> updatePixels());
+        Timer timer = new Timer(1, e -> updatePixels());
         timer.start();
     }
 
@@ -56,9 +78,10 @@ public class IsingModelVisualizer extends JFrame {
 //                }
 //            }
 //        }
-        xcoord = random.nextInt(150);
-        ycoord = random.nextInt(150);
-        if (random.nextDouble() < BLUE_THRESHOLD) {
+        xcoord = random.nextInt(GRID_SIZE);
+        ycoord = random.nextInt(GRID_SIZE);
+        double flipPos = isingCalculation(xcoord, ycoord);
+        if (random.nextDouble() > flipPos) { // Legacy code: if (random.nextDouble() < BLUE_THRESHOLD)
             gridPanels[xcoord][ycoord].setBackground(Color.BLUE);
         } else {
             gridPanels[xcoord][ycoord].setBackground(Color.RED);
@@ -66,7 +89,50 @@ public class IsingModelVisualizer extends JFrame {
         
     }
     
-    private void isingCalculation() {
+    private double isingCalculation(int x, int y) {
+    	int numPos = 0;
+    	int numNeg = 0;
+    	
+		// CHECK HORIZONTAL NEIGHBORS
+		if(y>0) {
+			if(grid[x][y-1] == POS) {
+				numPos++;
+			}
+			else {
+				numNeg++;
+			}
+		}
+		if(y<GRID_SIZE-1) {
+			if(grid[x][y+1] == POS) {
+				numPos++;
+			}
+			else {
+				numNeg++;
+			}
+		}
+		
+		// CHECK VERTICAL NEIGHBORS
+		if(x>0) {
+			if(grid[x-1][y] == POS) {
+				numPos++;
+			}
+			else {
+				numNeg++;
+			}
+		}
+		if(x<GRID_SIZE-1) {
+			if(grid[x+1][y] == POS) {
+				numPos++;
+			}
+			else {
+				numNeg++;
+			}
+		}
+		
+		// DO CALCULATION
+//		System.out.println("Prob: " + Math.exp(BETA*numPos)/(Math.exp(BETA*numPos) + Math.exp(BETA*numNeg)));
+		
+		return (Math.exp(BETA*numPos)/(Math.exp(BETA*numPos) + Math.exp(BETA*numNeg)));
     	
     }
 
